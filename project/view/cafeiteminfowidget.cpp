1f9b18cd17e8d253c98083d9d358808b5730a18b
Lake020 <leolake2002@gmail.com>
2023-01-29 11:11:28 +0100

    .

--- project/view/cafeiteminfowidget.cpp
+++ project/view/cafeiteminfowidget.cpp
@@ -3,7 +3,7 @@
 
 CafeItemInfoWidget::CafeItemInfoWidget(CafeItem* item, QWidget *parent): QWidget{parent}, item_{item}{
     setWindowTitle("Info");
-    setFixedSize(220, 310);
+    setFixedSize(QSize(400, 310));
     CafeWidgetVisitor visitor;
     item_->accept(visitor);
     QVBoxLayout* layout = new QVBoxLayout;
